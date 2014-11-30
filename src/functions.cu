e30e115bc123a5554a8d636d3a9fd05e231bc1a5
IL90 <arkhipovsky.ilya@yandex.ru>
2014-11-30 16:51:17 +0300

    added print properties

--- src/functions.cu
+++ src/functions.cu
@@ -11,10 +11,52 @@ void hnd(const cudaError_t &err) {
 		cout<<"cudaError_t = "<<cudaGetErrorString(err)<<endl;
 }
 
+void print(const cudaDeviceProp &prop) 
+{
+	cout<<"\n###Device Properties###"<<endl;
+	
+	cout<<"Name: "<<prop.name<<endl;
+	cout<<"Capability: "<<prop.major<<"."<<prop.minor<<endl;
+	cout<<"Clock Rate: "<<prop.clockRate<<endl;
+	cout<<"Overlap: "<<prop.deviceOverlap<<endl;
+	cout<<"Timeout Enabled: "<<prop.kernelExecTimeoutEnabled<<endl;
+
+	cout<<"Total Global Memory: "<<prop.totalGlobalMem<<endl;
+	cout<<"Total Constant Memory: "<<prop.totalConstMem<<endl;
+	cout<<"Max Memory Pitch: "<<prop.memPitch<<endl;
+	cout<<"Texture Alignment: "<<prop.textureAlignment<<endl;
+
+	cout<<"MultiProcessor Count: "<<prop.multiProcessorCount<<endl;
+	cout<<"Shared Memory per MP: "<<prop.sharedMemPerBlock<<endl;
+	cout<<"Registers per Block: "<<prop.regsPerBlock<<endl;
+	cout<<"Warp Size: "<<prop.warpSize<<endl;
+	cout<<"Max Threads per Block: "<<prop.maxThreadsPerBlock<<endl;
+
+	cout<<"Max Threads Dim ("<<prop.maxThreadsDim[0]<<", "<<prop.maxThreadsDim[1]<<", "<<prop.maxThreadsDim[2]<<")"<<endl;
+	cout<<"Max Grid Size ("<<prop.maxGridSize[0]<<", "<<prop.maxGridSize[1]<<", "<<prop.maxGridSize[2]<<")"<<endl;
+}
+
+void properties() 
+{
+	cout<<"properties"<<endl;
+	
+	int count;
+	hnd( cudaGetDeviceCount( &count ) );
+	
+	cudaDeviceProp prop;
+
+	for (int i = 0; i < count; ++i) 
+	{
+		hnd( cudaGetDeviceProperties( &prop, i ) );
+		print(prop);
+	}
+
+}
+
 void funcCuda( float *v1, int N1, float *v2, int N2, float* out, int N3 )
 {
 	typedef Opener<float, cudaMalloc, cudaFree> FloatOpener;
-	
+	properties();
 	Timer timer;
 	timer.start();
 
